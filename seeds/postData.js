f3912cd45e2caabf64f5a21ab2bd25e7e17b2f37
Clown Supreme <zachary.h.shedd@gmail.com>
2023-03-09 09:47:48 -0800

    Added to index, post data, and userdata in seeds folder

---
+++ seeds/postData.js
@@ -0,0 +1,27 @@
+const { Post } = require("../models");
+
+const postdata = [
+  {
+    title: "Gobbers Bloopers",
+    content: "I am a goober.",
+    user_id: "1",
+  },
+  {
+    title: "Who is Rob?",
+    content:
+      "Rob Endelman is a chef who, in addition to teaching cooking technique, empowers people with the knowledge to make better choices when it comes to buying and preparing food. He is also kinda silly about having someone who knows.",
+    user_id: "2",
+  },
+  {
+    title: "Darker",
+    content: "The darkest darkness.",
+    user_id: "3",
+  },
+  {
+    title: "HTML is cool.",
+    content: "I like html.",
+    user_id: "4",
+  },
+];
+
+module.exports = seedPost;
