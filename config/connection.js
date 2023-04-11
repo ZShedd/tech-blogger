a341580c6e67d3c99f7a5e18f4bae7913ab0cb22
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-11 12:22:22 -0700

    Added code for jawsdb mysql to run

--- config/connection.js
+++ config/connection.js
@@ -3,19 +3,34 @@ require('dotenv').config();
 
 let sequelize;
 
+var mysql = require('mysql')
+var connection
+
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
+  connection = mysql.createConnection({
       host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
+      user: 'root',
+      password: 'hacktheplanet',
+      database: 'todoagain_db'
     }
   );
 }
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     }
+//   );
+// }
 
 module.exports = sequelize;
