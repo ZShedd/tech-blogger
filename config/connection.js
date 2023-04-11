432c869eeec1ba474ea5ea4414df6b335c197be4
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-11 12:31:07 -0700

    Changes to deploy on Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,15 @@
+
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-let sequelize;
-
-var mysql = require('mysql')
-var connection
-
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      user: 'root',
-      password: 'hacktheplanet',
-      database: 'todoagain_db'
-    }
-  );
-}
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PW,
-//     {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3306,
-//     }
-//   );
-// }
+      dialect: 'mysql',
+      port: 3306
+    });
 
 module.exports = sequelize;
